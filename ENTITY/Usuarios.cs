8599ca65c056d2b90d07ba2901f10c66898e7e6b
hecttor129 <hectorcarrillom12@gmail.com>
2026-01-25 21:54:05 -0500

    test push

--- ENTITY/Usuarios.cs
+++ ENTITY/Usuarios.cs
@@ -8,9 +8,10 @@ namespace ENTITY
 {
     public class Usuarios
     {
-
-
-
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
 
 
 
